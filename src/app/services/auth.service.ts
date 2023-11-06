ac220e210142dc5b82e11b55380768cbdb24bb30
edwborges <eduardo.borges19646201@gmail.com>
2023-11-06 17:09:03 -0300

    Autenticação de LOGIN

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Credenciais } from './../models/credenciais';
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../config/api.config';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
+
+  constructor(private http: HttpClient) { }
+
+  authenticate(credenciais: Credenciais) {
+    return this.http.post(`${API_CONFIG.baseUrl}/login`, credenciais, {
+      observe: 'response',
+      responseType: 'text'
+    })
+  }
+
+  sucessfullLogin(authToken: string) {
+    localStorage.setItem('token', authToken);
+  }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token');
+    if(token != null) {
+      return !this.jwtService.isTokenExpired(token);
+    }
+    return false;
+  }
+}
