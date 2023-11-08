0cc939a3eff4d914d656f07a6e5111abc77e1c82
edwborges <eduardo.borges19646201@gmail.com>
2023-11-08 14:53:33 -0300

    Finalizando componente TECNICO-LIST

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { TecnicoService } from './../../../services/tecnico.service';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Page } from 'src/app/models/page';
 import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
@@ -8,26 +10,31 @@ import { Tecnico } from 'src/app/models/tecnico';
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
-export class TecnicoListComponent {
+export class TecnicoListComponent implements OnInit {
 
-  ELEMENT_DATA: Tecnico[] = [
-    {
-      id: 1,
-      nome: 'Eduardo Borges',
-      cpf: '123.456.789.10',
-      email: 'eduardo@email.com',
-      senha: '123456',
-      perfis: ['0'],
-      dataCriacao: '03/11/2023'
-    }
-  ];
+  ELEMENT_DATA: Tecnico[] = [];
 
   displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  constructor(private tecnicoService: TecnicoService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(page?: number, size?: number) {
+    this.tecnicoService.findAll(page, size).subscribe((resposta: Page<Tecnico>) => {
+      this.ELEMENT_DATA = resposta.content; 
+      this.dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
---
+++ src/app/services/tecnico.service.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../config/api.config';
+import { Tecnico } from '../models/tecnico';
+import { Page } from '../models/page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TecnicoService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(page?: number, size?: number): Observable<Page<Tecnico>> {
+    return this.http.get<Page<Tecnico>>(`${API_CONFIG.baseUrl}/tecnicos?page=${page ?? 0}&size=${size ?? 10}`)
+  }
+}
