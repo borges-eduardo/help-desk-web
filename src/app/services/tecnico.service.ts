7c084110e0c00c875fd271b55a9192d4f0133827
edwborges <eduardo.borges19646201@gmail.com>
2023-11-09 14:21:36 -0300

    Componente TECNICO-UPDATE implementado

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -12,11 +12,19 @@ export class TecnicoService {
 
   constructor(private http: HttpClient) { }
 
+  findById(id: any): Observable<Tecnico> {
+    return this.http.get<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${id}`);
+  }
+
   findAll(page?: number, size?: number): Observable<Page<Tecnico>> {
     return this.http.get<Page<Tecnico>>(`${API_CONFIG.baseUrl}/tecnicos?page=${page ?? 0}&size=${size ?? 10}`)
   }
 
   create(tecnico: Tecnico): Observable<Tecnico> {
     return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
+  }
+
+  update(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.put<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${tecnico.id}`, tecnico);
   }
 }
